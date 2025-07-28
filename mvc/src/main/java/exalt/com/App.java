4de433b1f974b565d0d41453687c31975541bbc6
Yaman Abu Jazar <yamanabujazar@gmail.com>
2025-07-28 15:48:13 +0300

    writing the MVC design pattern

---
+++ mvc/src/main/java/exalt/com/App.java
@@ -0,0 +1,29 @@
+package exalt.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        Student model = retriveStudentFromDatabase();
+
+        StudentView view = new StudentView();
+
+        StudentController controller = new StudentController(model, view);
+
+        controller.updateView();
+
+        controller.setStudentName("Vikram Sharma");
+
+        controller.updateView();
+    }
+
+    private static Student retriveStudentFromDatabase() {
+        Student student = new Student();
+        student.setName("Lokesh Sharma");
+        student.setRollNo("15UCS157");
+        return student;
+    }
+}
